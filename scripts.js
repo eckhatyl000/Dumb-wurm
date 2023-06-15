9a927bce83add9158add99be1751f71ea0f7b87b
Tyler Eckhardt <111406810+eckhatyl000@users.noreply.github.com>
2023-06-15 09:56:15 -0600

    adding in objectTypes

--- scripts.js
+++ scripts.js
@@ -1,50 +1,63 @@
-function rollDice() {
-    const diceType = document.getElementById('diceType').value;
-    const diceAmount = document.getElementById('diceAmount').value;
-    const outputContainer = document.getElementById('diceOutput');
+let objectEffects = {
+    "Object1": {
+        good: ["Good Effect 1-1", "Good Effect 1-2", "Good Effect 1-3"],
+        bad: ["Bad Effect 1-1", "Bad Effect 1-2", "Bad Effect 1-3"]
+    },
+    "Object2": {
+        good: ["Good Effect 2-1", "Good Effect 2-2", "Good Effect 2-3"],
+        bad: ["Bad Effect 2-1", "Bad Effect 2-2", "Bad Effect 2-3"]
+    },
+    // and so on for each object type...
+};
 
 
-    outputContainer.innerHTML = '';
+document.getElementById('form').addEventListener('submit', function (event) {
+    event.preventDefault(); 
+
+    let objectType = document.getElementById('objectType').value;
+    let objectNum = document.getElementById('objectNum').value;
+
+    let results = calculateEffects(objectType, objectNum);
+
+    document.getElementById('result').innerHTML = `Good Effects: ${results.goodEffectsResult.join(', ')}, Bad Effects: ${results.badEffectsResult.join(', ')}, Damage: ${results.damage}`;
 
 
-    if (diceType === '' || diceAmount === '') {
-        displayError('Please select a dice type and enter the amount.');
-        return;
+function calculateEffects(objectType, objectNum) {
+    
+    let goodEffectsArray = objectEffects[objectType].good;
+    let badEffectsArray = objectEffects[objectType].bad;
+
+    let goodEffectsResult = [];
+    let badEffectsResult = [];
+    let damage = 0;
+
+    let baseGoodBadEffectPercentage = 0.25;
+    let baseDamagePercentage = 0.50;
+
+    for (let i = 0; i < objectNum; i++) {
+        let goodBadEffectChance = Math.min(baseGoodBadEffectPercentage + (i * 0.01), 1);
+        let damageChance = Math.min(baseDamagePercentage + (i * 0.01), 1);
+
+        let randGoodBad = Math.random();
+        let randDamage = Math.random();
+
+        if (randGoodBad < goodBadEffectChance) {
+            let chosenGoodEffect = goodEffectsArray[Math.floor(Math.random() * goodEffectsArray.length)];
+            if (!goodEffectsResult.includes(chosenGoodEffect)) {
+                goodEffectsResult.push(chosenGoodEffect);
+            }
+
+            let chosenBadEffect = badEffectsArray[Math.floor(Math.random() * badEffectsArray.length)];
+            if (!badEffectsResult.includes(chosenBadEffect)) {
+                badEffectsResult.push(chosenBadEffect);
+            }
+        }
+
+        if (randDamage < damageChance) {
+            damage += 1;
+        }
     }
 
-    if (!Number.isInteger(+diceAmount) || +diceAmount <= 0) {
-        displayError('Please enter a valid positive number for dice amount.');
-        return;
-    }
-
-    const validDiceTypes = ['D4', 'D6', 'D8', 'D10', 'D12', 'D20', 'D100'];
-    if (!validDiceTypes.includes(diceType)) {
-        displayError('Please select a valid dice type.');
-        return;
-    }
-
-
-    let total = 0;
-    let rolls = [];
-
-    for (let i = 0; i < diceAmount; i++) {
-        const roll = Math.floor(Math.random() * parseInt(diceType.slice(1))) + 1;
-        rolls.push(roll);
-        total += roll;
-    }
-
-
-    const resultHTML = document.createElement('div');
-    resultHTML.innerHTML = `
-    <p>Rolling ${diceAmount} ${diceType}(s)...</p>
-    <p>Results: ${rolls.join(', ')}</p>
-    <p>Total: ${total}</p>
-  `;
-
-    outputContainer.appendChild(resultHTML);
+    return { goodEffectsResult, badEffectsResult, damage };
 }
 
-function displayError(message) {
-    const errorContainer = document.getElementById('errorContainer');
-    errorContainer.textContent = message;
-}
